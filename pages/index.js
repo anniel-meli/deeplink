aedf904f2c729beb0431c7f8102aaa5d4a3a1aab
Leonardo Almeida <leonardo.almeida18@gmail.com>
2022-04-27 23:18:38 -0300

    changes 4

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const deeplinkBeta = `mercadopago://webview/?url=${encodeURIComponent(
-    "https://beta.mercadopago.com.br/tools/list&navigation_icon=boton-tulink"
+    "https://beta.mercadopago.com.br/tools/list"
   )}&hides_bottom_bar=true&navigation_icon=boton-tulink&use_web_title=false`;
   const deeplinkOmega = `mercadopago://webview/?url=${encodeURIComponent(
-    "https://omega.mercadopago.com.br/tools/list&navigation_icon=boton-tulink"
+    "https://omega.mercadopago.com.br/tools/list"
   )}&hides_bottom_bar=true&navigation_icon=boton-tulink&use_web_title=false`;
   return (
     <div className={styles.container}>
